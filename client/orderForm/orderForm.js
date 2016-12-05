78d76a3a65f8f71edf4e9f8283fd62d8795f8541
krillian11 <asw7731@gmail.com>
2016-12-05 08:39:24 -0500

    Added stuff to the order form

--- client/orderForm/orderForm.js
+++ client/orderForm/orderForm.js
@@ -2,8 +2,10 @@ import {Template} from "meteor/templating";
 import "./orderForm.html";
 
 Template.orderForm.events({
-  "submit #orderForm"(event){
+  "submit #printOrder"(event){
     //prevents default form submission
     event.preventDefault();
+    console.log("Hi.");
+    Meteor.call("submitOrderForm");
   },
 });
