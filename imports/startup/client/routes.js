81ce65e7fc467ea5300be54e31f82828290bffa2
JrodManU <jrodmanu@yahoo.com>
2017-01-06 09:41:23 -0500

    home page and layouts work!

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,6 +1,18 @@
+import { FlowRouter } from "meteor/kadira:flow-router";
+import { BlazeLayout } from "meteor/kadira:blaze-layout";
+
+import "../../ui/layouts/mainLayout/mainLayout.js";
+
+import "../../ui/components/footer/footer.js";
+import "../../ui/components/header/header.js";
+import "../../ui/components/navigation/navigation.js";
+import "../../ui/components/orderForm/orderForm.js";
+import "../../ui/components/orderFormLinksTemplate/orderFormLinksTemplate.js";
+import "../../ui/components/orderFormLinkTemplate/orderFormLinkTemplate.js";
+
 FlowRouter.route("/", {
   action: function() {
     console.log("Wew lad");
-    BlazeLayout.render("mainLayout");
+    BlazeLayout.render("mainLayout", {page: "homePage"});
   }
 });
