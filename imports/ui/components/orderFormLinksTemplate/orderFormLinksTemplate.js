c883bee67d79f29560bba1aeb3224e2ab08fe675
krillian11 <asw7731@gmail.com>
2017-01-09 07:44:20 -0500

    added to orderFormLinksTemplate

--- imports/ui/components/orderFormLinksTemplate/orderFormLinksTemplate.js
+++ imports/ui/components/orderFormLinksTemplate/orderFormLinksTemplate.js
@@ -1,1 +1,9 @@
 import "./orderFormLinksTemplate.html";
+import {Meteor} from "meteor/meteor";
+
+Meteor.subscribe("orderFormLinks");
+Template.orderFormLinkTemplate.helpers({
+  "orderFormLinks": function(){
+      return OrderFormLinks.find();
+  }
+});
