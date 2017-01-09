44987e9ea96e2baa66ad7f945a959d2458c4a877
JrodManU <jrodmanu@yahoo.com>
2017-01-09 08:58:59 -0500

    Fixed naming in orderFormLinksTemplate

--- imports/ui/components/orderFormLinksTemplate/orderFormLinksTemplate.js
+++ imports/ui/components/orderFormLinksTemplate/orderFormLinksTemplate.js
@@ -2,7 +2,8 @@ import "./orderFormLinksTemplate.html";
 import {Meteor} from "meteor/meteor";
 
 Meteor.subscribe("orderFormLinks");
-Template.orderFormLinkTemplate.helpers({
+
+Template.orderFormLinksTemplate.helpers({
   "orderFormLinks": function(){
       return OrderFormLinks.find();
   }
