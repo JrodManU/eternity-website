86b24a7bf7dd74bd5d779cb97d485db2d1c3669e
Wesam Haddad <wesam.haddad14@gmail.com>
2017-01-19 07:44:38 -0500

    Worked on the NavBar

--- imports/ui/components/navigation/navigation.js
+++ imports/ui/components/navigation/navigation.js
@@ -1,2 +1,16 @@
 import "./navigation.html";
 import "./navigation.css";
+
+Template.navigation.rendered = function() {
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the
+      //nav bar to stick.
+    if ($(window).scrollTop() > 280) {
+      $('#navigation').addClass('navbarFixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('#navigation').removeClass('navbarFixed');
+    }
+  });
+}
