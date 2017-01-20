9d774f05fff2a0db3b5486d590b943bea7c2d199
Elijah Woodard <Elijah Woodard>
2017-01-20 01:34:53 -0500

    Added the html and commented the js for the html Form

--- imports/ui/pages/orderFormPage/orderFormPage.js
+++ imports/ui/pages/orderFormPage/orderFormPage.js
@@ -1,1 +1,20 @@
-import "./orderFormPage.html";
+ /*import "./orderFormPage.html";
+
+Meteor.subscribe("orderFormPage")
+
+Template.body.events({
+  "submit .orderFormTask"(event){
+//prevent default submit
+event.preventDefault();
+//get value from form element
+const target = event.target;
+const text = target.text.value;
+//insert task into current collection
+Tasks.insert({
+  text,
+  createdAt: new Date();
+});
+target.text.value="";
+};
+});
+*/
