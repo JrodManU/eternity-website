07dccaa1b12f5179383e6e640efdfef38c175285
JrodManU <jrodmanu@yahoo.com>
2017-01-25 18:14:01 -0500

    One can now submit order forms

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,4 +1,7 @@
 import "../../api/orderFormLinks/orderFormLinks.js";
+import "../../api/orders/methods.js";
 import "../../api/orderFormLinks/server/publications.js";
+
 import "../../api/orders/orders.js";
+import "../../api/orders/methods.js";
 import "../../api/orders/server/publications.js";
