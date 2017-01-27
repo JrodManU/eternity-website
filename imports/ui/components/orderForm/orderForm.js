e142a5f8205ca42d9a5a910295412dec394eebb1
JrodManU <jrodmanu@yahoo.com>
2017-01-27 08:25:56 -0500

    You pleb, there was a ;

--- imports/ui/components/orderForm/orderForm.js
+++ imports/ui/components/orderForm/orderForm.js
@@ -2,7 +2,7 @@ import { Template } from "meteor/templating";
 import "./orderForm.html";
 
 Template.orderForm.helpers({
-"order":function(){
-  return Orders.findOne({_id:FlowRouter.getParam("orderId")});
-};
+  "order":function(){
+    return Orders.findOne({_id:FlowRouter.getParam("orderId")});
+  }
 });
