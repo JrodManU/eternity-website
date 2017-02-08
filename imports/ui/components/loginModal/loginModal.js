9a743e4bde4ca3240933558361f216e859ab6d16
JrodManU <jrodmanu@yahoo.com>
2017-02-08 07:21:04 -0500

    added stuff for loginModal

--- imports/ui/components/loginModal/loginModal.js
+++ imports/ui/components/loginModal/loginModal.js
@@ -1,2 +1,11 @@
 import "./loginModal.css";
 import "./loginModal.html";
+
+import { Meteor } from "meteor/meteor";
+import { Template } from "meteor/templating";
+
+Template.loginModal.helpers({
+  "showLoginModal":function() {
+    return Session.get("showLoginModal");
+  }
+});
