fe10f8f2145959badd1724f7cccb9ce417bc6b6e
WesamHaddad125 <wesam.haddad14@gmail.com>
2017-03-01 21:59:12 -0500

    worked on css files and html files

--- imports/ui/components/loginModalContent/loginModalContent.js
+++ imports/ui/components/loginModalContent/loginModalContent.js
@@ -13,19 +13,19 @@ Template.loginModalContent.events({
     var email = event.target.loginEmail.value;
     var password = event.target.loginPassword.value;
     if(!email.trim()) {
-      MeteorAlerts.alert("Please enter a username", 2000, []);
+      MeteorAlerts.alert("Please enter a Username", 2000, ["meteorAlertWarning"]);
       return;
     }
     console.log()
     if(!password.trim()) {
-      MeteorAlerts.alert("Please enter a password", 2000, []);
+      MeteorAlerts.alert("Please enter a password", 2000, ["meteorAlertWarning"]);
       return;
     }
     Meteor.loginWithPassword(email, password, function(error) {
       if(error) {
-        MeteorAlerts.alert("User does not exist or password is incorrect.", 2000, []);
+        MeteorAlerts.alert("User does not exist or password is incorrect.", 2000, ["meteorAlertWarning"]);
       } else {
-        MeteorAlerts.alert("Successfully logged in!", 2000, []);
+        MeteorAlerts.alert("Successfully logged in!", 2000, ["meteorAlertSuccess"]);
         Session.set("showLoginModal", false);
       }
     });
