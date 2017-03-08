4b0439fe1718c20e19ea06e1f9827553952de36b
JrodManU <jrodmanu@yahoo.com>
2017-03-07 21:54:50 -0500

    excluded original admin from publication for admins

--- imports/ui/components/allOrders/allOrders.js
+++ imports/ui/components/allOrders/allOrders.js
@@ -1,1 +1,9 @@
+import { Template } from "meteor/templating";
+
 import "./allOrders.html";
+
+Template.allOrders.helpers({
+  "orders":function() {
+    return Orders.find({markedForReview: true});
+  }
+})
--- imports/ui/components/allUsers/allUsers.js
+++ imports/ui/components/allUsers/allUsers.js
@@ -1,1 +1,12 @@
+import { Meteor } from "meteor/meteor";
+import { Template } from "meteor/templating";
+
 import "./allUsers.html";
+
+Meteor.subscribe("users");
+
+Template.allUsers.helpers({
+  "users":function() {
+    return Meteor.users.find({});
+  }
+})
