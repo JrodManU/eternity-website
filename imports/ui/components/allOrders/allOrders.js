fc653f959e1159eb2f41d1cc4c2d4637961728af
JrodManU <jrodmanu@yahoo.com>
2017-03-13 07:45:03 -0400

    edited table on admin page

--- imports/ui/components/allOrders/allOrders.js
+++ imports/ui/components/allOrders/allOrders.js
@@ -8,11 +8,17 @@ Template.allOrders.helpers({
       {markedForReview: true},
       {reviewed: false}
     ]});
+  },
+  "email":function() {
+    return Meteor.users.findOne({_id: this.userId}).emails[0].address;
   }
 });
 
 Template.allOrders.events({
   "click .markAsReviewed":function() {
     Meteor.call("markOrderAsReviewed", this._id);
+  },
+  "click .viewOrder":function() {
+    FlowRouter.go("orderView", {orderId: this._id});
   }
 });
