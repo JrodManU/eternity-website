696d0df458359ea9909f876b80e01696bd15fe88
WesamHaddad125 <wesam.haddad14@gmail.com>
2017-03-19 00:52:27 -0400

    worked on NavBar

--- imports/ui/components/navigation/navigation.js
+++ imports/ui/components/navigation/navigation.js
@@ -17,3 +17,14 @@ Template.navigation.rendered = function() {
     }
   });
 }
+
+$(document).ready(function() {
+    $( '.dropdown' ).hover(
+        function(){
+            $(this).children('.sub-menu').slideDown(200);
+        },
+        function(){
+            $(this).children('.sub-menu').slideUp(200);
+        }
+    );
+}); // end ready
