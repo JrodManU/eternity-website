7a2fb7b87ead871d15ae8d900812bbbb51942fd2
JrodManU <jrodmanu@yahoo.com>
2017-03-20 15:43:43 -0400

    slideshow fixed

--- imports/ui/components/slideshow/slideshow.js
+++ imports/ui/components/slideshow/slideshow.js
@@ -4,9 +4,13 @@ import "./slideshow.css";
 import { Template } from "meteor/templating";
 
 Template.slideshow.onCreated(function() {
-  window.setInterval(function() {
+  this.interval = window.setInterval(function() {
     var current = $("#slideshow img:nth-child(1)");
     var next = $("#slideshow img:nth-child(2)");
     $("#slideshow").append(current);
   }, 1000);
+});
+
+Template.slideshow.onDestroyed(function() {
+  window.clearInterval(this.interval);
 });
