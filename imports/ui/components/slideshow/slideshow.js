98bbe02253cd92463c3896d27947ad242e135910
JrodManU <jrodmanu@yahoo.com>
2017-03-20 10:05:04 -0400

    slideshow

--- imports/ui/components/slideshow/slideshow.js
+++ imports/ui/components/slideshow/slideshow.js
@@ -3,16 +3,10 @@ import "./slideshow.css";
 
 import { Template } from "meteor/templating";
 
-Template.slideshow.onRendered(function() {
-  console.log("hi");
-  this.$("#slideshow > div:gt(0)").hide();
-
-  setInterval(function() {
-    this.$('#slideshow > div:first')
-      .hide()
-      .next()
-      .show()
-      .end()
-      .appendTo('#slideshow');
-  }, 5000);
+Template.slideshow.onCreated(function() {
+  window.setInterval(function() {
+    var current = $("#slideshow img:nth-child(1)");
+    var next = $("#slideshow img:nth-child(2)");
+    $("#slideshow").append(current);
+  }, 1000);
 });
