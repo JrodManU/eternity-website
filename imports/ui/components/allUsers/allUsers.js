4a702210a14f50b28063a3923facb0323bcf5cc1
JrodManU <jrodmanu@yahoo.com>
2017-03-22 16:14:06 -0400

    added search bar

--- imports/ui/components/allUsers/allUsers.js
+++ imports/ui/components/allUsers/allUsers.js
@@ -1,13 +1,18 @@
 import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
+import { Session } from "meteor/session";
 
 import "./allUsers.html";
 
 Meteor.subscribe("users");
 
+Template.allUsers.onCreated(function() {
+  Session.set("usersSearchFilter", "");
+});
+
 Template.allUsers.helpers({
   "users":function() {
-    return Meteor.users.find({});
+    return Meteor.users.find({emails: {$elemMatch: {address: {$regex: ".*" + Session.get("usersSearchFilter") + ".*"}}}});
   },
   "iteratedUserIsInRole":function(role) {
     return Roles.userIsInRole(this._id, role);
@@ -23,5 +28,9 @@ Template.allUsers.events({
   },
   "click .deleteUser":function() {
     Meteor.call("deleteUser", this._id);
+  },
+  "submit #usersSearchForm":function(event, template) {
+    event.preventDefault();
+    Session.set("usersSearchFilter", template.$("#usersSearchField").val().trim());
   }
 });
